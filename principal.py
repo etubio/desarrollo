4a8e10c6650fd29ab138fd9d594bd4e9b1142d25
root <root@debian>
2015-08-05 06:09:44 +0200

    estructuración

--- principal.py
+++ principal.py
@@ -7,44 +7,48 @@ from Host import Host, Port
 
 def print_usage():
 	print 'Usage: ./principal.py <red>\n\
-	Example: ./principal.py 192.168.1.0/24\n\
-	Example: python principal.py 192.168.1.10/32'
+	Example 1: ./principal.py 192.168.1.0/24\n\
+	Example 2: python principal.py 192.168.1.10/32'
+	
 	
 def check_key(k,l):
-	encontrado = False
 	for e in l:
 		if e == k:
-			encontrado = True
-	return encontrado
+			return True
+	return False
 
-def host_discovery(red):
+	
+def host_discovery(nm,red):
 	print 'Barrido ping (ARP en red local)'
 	hosts_up_list = nm.scan(red,arguments='-sP')['scan'].keys()
 	print 'Terminado, '+str(len(hosts_up_list))+' host(s) activo(s) encontrado(s)'
 	return hosts_up_list
 	
-def dns_fingerprinting(ip,h):
+	
+def dns_fingerprinting(nm,ip,h):
 	print 'Escaneo de lista (DNS) para el host '+ip
 	result_dns = nm.scan(ip,arguments='-sL')['scan']
 	print 'Terminado'
 	h.dns = result_dns[ip]
 
-def port_scanning(ip,h):
+	
+def port_scanning(nm,ip,h):
 	print 'Port scanning para el host '+ip
 	result = nm.scan(ip,arguments='-PN')
 	print 'Terminado'
 	# check keys
-	if check_key(str(ip),result['scan'].keys()):
-		if check_key('tcp',result['scan'][str(ip)].keys()):
-			found_ports = result['scan'][str(ip)]['tcp'].keys()
+	if check_key(ip,result['scan'].keys()):
+		if check_key('tcp',result['scan'][ip].keys()):
+			found_ports = result['scan'][ip]['tcp'].keys()
 			for e in found_ports:
-				port = result['scan'][str(ip)]['tcp'][e]	
+				port = result['scan'][ip]['tcp'][e]	
 				p = Port('tcp',str(e),port['state'],port['name'])
 				h.ports.append(p)
 	
-def os_fingerprinting(ip,h):
+	
+def os_fingerprinting(nm,ip,h):
 	print 'OS fingerprinting para el host '+ip
-	result = nm.scan(ip,arguments='-O')['scan'][str(ip)]
+	result = nm.scan(ip,arguments='-O')['scan'][ip]
 	print 'Terminado'
 	h.hostname = result['hostname']
 	# mac = result['addresses']['mac']
@@ -53,6 +57,7 @@ def os_fingerprinting(ip,h):
 		if check_key('vendor',result['osclass']):
 			h.os = result['osclass']['vendor'] + ' ' + result['osclass']['osfamily']
 
+			
 def to_pdf(diccionario,title):
 	chapter.title = title
 	pdf = chapter.PDF()
@@ -64,43 +69,43 @@ def to_pdf(diccionario,title):
 	pdf.output('informe.pdf','F')
 	
 	
-diccionario = {}
+def main():
+	# check parameters
+	if (len(sys.argv) != 2):
+		print_usage()
+		exit()	
 
-# check parameters
-if (len(sys.argv) != 2):
-	print_usage()
-	exit()	
+	red = sys.argv[1]
+	print 'Analizando la red '+red
+	nm = nmap.PortScanner()
 
-red = sys.argv[1]
-print 'Analizando la red '+red
-nm = nmap.PortScanner()
+	# Host Discovery
+	hosts_up_list = host_discovery(nm,red)
 
-# Host Discovery
-hosts_up_list = host_discovery(red)
+	diccionario = {}
+	for ip in hosts_up_list:
+		h = Host(ip)
+		
+		# DNS Fingerprinting
+		dns_fingerprinting(nm,ip,h)
+		
+		# Port Scanning
+		port_scanning(nm,ip,h)
+		
+		# OS Fingerprinting
+		os_fingerprinting(nm,ip,h)	
+		
+		# Port fingerprinting
+		
+		
+		print h
+		diccionario[ip] = h
 
-for ip in hosts_up_list:
-	h = Host(ip)
-	
-	# DNS Fingerprinting
-	dns_fingerprinting(ip,h)
-	
-	# Port Scanning
-	port_scanning(ip,h)
-	
-	# OS Fingerprinting
-	os_fingerprinting(ip,h)	
-	
-	# Port fingerprinting
-	
-	
-	print h
-	diccionario[ip] = h
-
-# to PDF
-to_pdf(diccionario,red)
+	# to PDF
+	to_pdf(diccionario,red)
 
 
-
+main()
 
 
 
